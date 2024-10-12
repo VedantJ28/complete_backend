cdd61a85be273e573abe5bdc6f69e4c740b2ba3f
Vedant Jadhav <vedantjadhav324@gmail.com>
2024-10-12 18:49:35 +0530

    Adde User and video model

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import dotenv from "dotenv";
-import dbConnect from './db/db.js'
-import express from "express";
+import dbConnect from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
-const app = express();
+dbConnect()
+  .then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Listning on port : ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error!!!", error);
+  });
 
-dbConnect();
-
-app.get("/", (req, res) =>{
-    res.send("<h1>Hello World!</h1>");
-})
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Listning on port : ${PORT}`); 
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
