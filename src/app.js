fd0ae9ceba9c4dc90cca605d98b6f5ca1645ef60
Vedant Jadhav <vedantjadhav324@gmail.com>
2024-10-13 11:41:23 +0530

    Debugging and testing sample routes

--- src/app.js
+++ src/app.js
@@ -12,4 +12,17 @@ app.use(express.json({ limit:"16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true}));
 app.use(express.static("Public"));
 
+
+//Routes import
+import userRouter from "./routes/user.route.js"
+
+
+//Routes Declaration
+// app.get("/", (req, res) => {
+//     res.send("Welcome to the API");
+// })
+
+app.use("/api/v1/users", userRouter);
+
+
 export { app };
