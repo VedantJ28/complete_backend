19bd585dbef49d58473fd4710c4b55275edfa789
Vedant Jadhav <vedantjadhav324@gmail.com>
2024-10-13 22:25:10 +0530

    added access token refresh controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/coudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+import jwt from "jsonwebtoken";
 
 const genrateAccessAndRefreshToken = async (userId) => {
     try {
@@ -174,4 +175,51 @@ const logoutUser = asyncHandler(async (req, res) =>{
 
 });
 
-export { registerUser, loginUser, logoutUser };
+const accessTokenRefresh = asyncHandler(async (req, res) =>{
+    const incomingRefreshToken = req.cookies?.refreshToken || req.body.refreshToken;
+
+    if(!incomingRefreshToken){
+        throw new ApiError(400, "Unauthorized request");
+    }
+
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+    
+        const user = await User.findById(decodedToken?._id);
+    
+        if(!user){
+            throw new ApiError(400, "Invalid token");
+        }
+    
+        if(decodedToken != user?.refreshToken){
+            throw new ApiError(400, "Token expired");
+        }
+    
+        const {accessToken, newRefreshToken} = await genrateAccessAndRefreshToken(decodedToken._id);
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    accessToken, refreshToken: newRefreshToken
+                },
+                "Access token refreshed"
+            )
+        );
+    } catch (error) {
+        throw new ApiError(
+            400,error?.message || "Error while refreshing access token"
+        )
+    } 
+});
+
+export { registerUser, loginUser, logoutUser, accessTokenRefresh };
